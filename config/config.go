880d74e42540c006fd31b3855dea017909750e81
J_ATEAM <wlsaud619@gmail.com>
2020-01-14 13:44:38 +0900

    v0.3.0

--- config/config.go
+++ config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"log"
-//	"time"
 
 	"github.com/BurntSushi/toml"
 	rpc "github.com/xiphiar/secret-validator_exporter/getData/rpc"
@@ -40,7 +39,7 @@ type configType struct {
 }
 
 
-func Init() {
+func Init() string {
 
 	Config = readConfig()
 
@@ -49,15 +48,13 @@ func Init() {
 
 	rest.OperAddr = Config.Validator.OperatorAddr
 
+	return Config.Options.ListenPort
 }
 
 func readConfig() configType {
 
         var config configType
 
-//	path := viper.GetString(ConfigPath)+"/config.toml"
-
-//        if _, err := toml.DecodeFile(path, &config); err != nil{
         if _, err := toml.DecodeFile(ConfigPath +"/config.toml", &config); err != nil{
 
                 log.Fatal("Config file is missing: ", config)
